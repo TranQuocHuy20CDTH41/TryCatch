3b277a0324080755d9a6a5b9719576588021e078
QuocHuy <huyhvker@gmail.com>
2021-06-14 22:41:55 +0700

    first commit

---
+++ src/advance/dev/MainApp.java
@@ -0,0 +1,40 @@
+package advance.dev;
+
+import java.util.Scanner;
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		System.out.println("Nhap vao so a:");
+		a = sc.nextInt();
+		System.out.println("Nhap vao so b:");
+		b = sc.nextInt();
+		
+		try {
+			int c = a/b;
+			if(b!=0) {
+				System.out.println(c);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e);
+		}
+		System.out.println("\n");
+		String A;
+		System.out.println("Nhap vao chuoi A:");
+		A = sc.next();
+		
+		try {
+			Integer.parseInt(A);
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e);
+			System.out.println("So nhap vao khong phai dang so");
+		}
+		
+		
+	}
+
+}
